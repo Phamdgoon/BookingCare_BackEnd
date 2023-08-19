64481548280372878c3cd496e8c521d380f7515b
Phamdgoon <phamdgoon1606@gmail.com>
2023-08-19 22:18:31 +0700

    api-login

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
+import cors from "cors";
 require("dotenv").config;
 
 let app = express();
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 
 //config app
 
